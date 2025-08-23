45856a4540e12448ddc7cfedee30dac44957715d
Rastacat94 <rastacat1960@gmail.com>
2025-08-23 09:59:33 -0500

    Update mock.js

--- src/mock.js
+++ src/mock.js
@@ -12,22 +12,6 @@ export const projects = [
     link: "https://restaurantes-lac.vercel.app/",
     github: "https://github.com/Rastacat94/restaurantes",
   },
-  {
-    name: "Tu segundo proyecto",
-    tagline:
-      "Una descripción corta y concisa para tu segundo proyecto. Explica qué hace y su objetivo.",
-    image: "https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg",
-    link: "", // Deja el link vacío, ya que no tienes un proyecto en vivo
-    github: "https://github.com/Rastacat94",
-  },
-  {
-    name: "Tu tercer proyecto",
-    tagline:
-      "Una descripción corta y concisa para tu tercer proyecto. Explica qué hace y su objetivo.",
-    image: "https://cdn.pixabay.com/photo/2017/08/07/23/50/computer-2609071_1280.jpg",
-    link: "", // Deja el link vacío, ya que no tienes un proyecto en vivo
-    github: "https://github.com/Rastacat94",
-  },
 ];
 export const contact = {
   email: "lopezvarelajuan@hotmail.com",
